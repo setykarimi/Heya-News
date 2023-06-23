ae7b988a220081aa5cff98639d6e8acb90502509
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-23 18:42:56 +0430

    Subject: Style, Description: Edit for responsive view, Date: 6/23/2023

--- src/components/banner/newsBanner.jsx
+++ src/components/banner/newsBanner.jsx
@@ -3,9 +3,9 @@ import Image from '../../assets/images/newsBanner.webp';
 const NewsBanner = () => {
     return (
         <div className='xl:w-9/12 lg:w-10/12 md:w-full mx-auto p-1 bg-white rounded-2xl lg:mt-32 md:mt-12 mt-10'>
-            <div className="lg:grid flex flex-col grid-cols-2">
-                <img src={Image} alt="NewsBanner" className='rounded-tl-2xl lg:rounded-bl-2xl lg:rounded-tr-none rounded-tr-2xl lg:h-full h-64 object-cover' />
-                <div className='rounded-tr-2xl rounded-br-2xl md:rounded-bl-none rounded-bl-2xl bg-white md:p-4 p-3 flex flex-col md:gap-0 gap-2 justify-between '>
+            <div className="md:grid flex flex-col grid-cols-2">
+                <img src={Image} alt="NewsBanner" className='rounded-tl-2xl md:rounded-bl-2xl lg:rounded-tr-none rounded-tr-2xl md:h-full h-64 object-cover' />
+                <div className='rounded-tr-2xl rounded-br-2xl md:rounded-bl-none rounded-bl-2xl bg-white md:p-4 p-3 flex flex-col gap-2 justify-between '>
                     <span className="font-light bg-green-600 text-white w-fit px-2 py-1 rounded-full text-xs block">FEATURED</span>
                     <h4 className='text-gray-950 font-bold  text-2xl '>Cheap Airline Tickets Great Ways To Save</h4>
                     <p className='text-gray-800 font-light text-justify'>In this digital generation where information can be easily obtained within seconds, business cards since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
