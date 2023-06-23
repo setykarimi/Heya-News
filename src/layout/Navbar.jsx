9a1a2cefc1e8758bf164b7c5039a66e2aeb2d49b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-23 19:15:47 +0430

    Subject: Layout, Description: Edit layout, Date: 6/23/2023

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -32,20 +32,21 @@ const Navbar = () => {
     }
 
     return (
-        <nav className="xl:w-9/12 lg:w-10/12 md:w-full  mx-auto sticky text-white px-12 py-6 flex justify-between z-50 -mb-24">
-            <img src={Logo} alt='heya' className='w-16' />
+        <nav className="mx-auto sticky top-0 text-gray-900 bg-white px-12 py-4 z-50 ">
+            <div className="container mx-auto flex justify-between ">
+                <img src={Logo} alt='heya' className='w-16' />
 
-            <div className={`flex md:flex-row flex-col md:static fixed top-0 bottom-0 transition-all z-50 ${!showMenu ? "-right-56" : "right-0 p-4"} md:bg-transparent bg-white md:text-white text-gray-900 md:h-fit h-[103vh] gap-4 items-center`}>
-                {menuItem.map(({ title, link }) => <Link key={title} to={link} className='font-light'>{title}</Link>)}
-                <button className='border border-white py-2 px-6 ml-4 rounded-full font-medium'>Get it Now</button>
+                <div className={`flex md:flex-row flex-col md:static fixed top-0 bottom-0 transition-all z-50 ${!showMenu ? "-right-56" : "right-0 p-4"} md:bg-transparent bg-white md:text-gray-900 text-gray-900 md:h-fit h-[103vh] gap-4 items-center`}>
+                    {menuItem.map(({ title, link }) => <Link key={title} to={link} className='font-light'>{title}</Link>)}
+                    <button className='border border-white py-2 px-6 ml-4 rounded-full font-medium'>Get it Now</button>
+                </div>
+
+                <button className={`md:hidden ${showMenu ? "mr-32" : "mr-0"}`} onClick={showMenuHandler}>
+                    {showMenu ?
+                        <CloseCircle size="24" color="#FFFFFF" />
+                        : <Menu size="24" color="#FFFFFF" />}
+                </button>
             </div>
-
-            <button className={`md:hidden ${showMenu ? "mr-32": "mr-0"}`} onClick={showMenuHandler}>
-                {showMenu ?
-                <CloseCircle size="24" color="#FFFFFF"/>
-                : <Menu size="24" color="#FFFFFF" />}
-            </button>
-
         </nav>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,17 +7,17 @@ const Home = () => {
 
     return (
         <div>
-            <div style={{ background: `url(${Bg})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'top center' }} className='md:pt-60 pt-24 lg:px-10 px-4 bg-transparent'>
+            <div style={{ background: `url(${Bg})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'top center', backgroundSize: '100%' }} className='md:pt-60 pt-24 lg:px-10 px-4 bg-transparent'>
                 <SearchArticle />
                 <NewsBanner />
             </div>
 
-            <div className='xl:w-9/12 lg:w-10/12 md:w-full mx-auto lg:px-0 px-4'>
+           
                 <PopularArticles />
                 {/* <RecentArticles /> */}
                 {/* <CaseStudies /> */}
                 <TodayHeadlines />
-            </div>
+            
         </div>
     )
 }
