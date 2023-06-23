18cece2ac9573051e407c47726b6915108b0bc55
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-23 12:33:15 +0430

    Subject: all articles, Description: Remove link, Date: 6/23/2023

--- src/components/card/allArticles.jsx
+++ src/components/card/allArticles.jsx
@@ -1,13 +1,10 @@
 import { Link } from "react-router-dom";
 import motherHood from '../../assets/images/motherhood/motherhood.png';
-import girl from '../../assets/images/people/girlFour.png';
-import camping from '../../assets/images/travel/camping.png';
-import rent from '../../assets/images/travel/rent.png';
-import friends from '../../assets/images/university/freinds.png';
-import avatar1 from '../../assets/images/people/girlFour.png';
 import avatar2 from '../../assets/images/people/girlFive.png';
+import avatar1 from '../../assets/images/people/girlFour.png';
 import avatar3 from '../../assets/images/people/girlThree.png';
-import { CardArticle } from "./cardArticle";
+import camping from '../../assets/images/travel/camping.png';
+import friends from '../../assets/images/university/freinds.png';
 import CardExplore from "./cardExplore";
 
 const AllArticles = () => {
@@ -37,6 +34,7 @@ const AllArticles = () => {
             img: friends
         }
     ]
+
     return (
         <div className='mt-24'>
             <h2 className='text-3xl text-gray-800 font-medium'>Today Headlines</h2>
@@ -47,27 +45,7 @@ const AllArticles = () => {
             <div className="grid lg:md:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-2 mb-8">
                 {news.map(({type, date, title, avatar, author, img})=> <CardExplore 
                 type={type} date={date} title={title} avatar={avatar} author={author} img={img}/>)}
-                
             </div>
-            {/* <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8">
-                <CardArticle
-                    img={motherHood}
-                    title="Woman Up: It's about time we redefine what being a supermum means"
-                    author="Riha Sanire"
-                    date="3 december" />
-                <CardArticle
-                    img={girl}
-                    title="Get Mental Health Content from GoodTherapy.org in Your Inbox"
-                    author="Mina Sinai"
-                    date="4 october" />
-                <CardArticle
-                    img={friends}
-                    title="Get Mental Health Content from GoodTherapy.org in Your Inbox"
-                    author="Mina Sinai"
-                    date="4 october" />
-
-            </div> */}
-            <Link to='' className='mt-4 block w-fit mx-auto border border-blue-600 h-full text-blue-600 text-sm font-medium py-2 px-4 rounded-md'>More Article</Link>
         </div>
     );
 }
