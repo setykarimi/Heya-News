86bc4427ba3f744261d114714067cfb0dcb73948
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-24 22:01:36 +0430

    Subject: Homepage, Description: Remove search, Date: 6/24/2023

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,15 @@
-import Bg from '../assets/images/Background.png';
 import NewsBanner from '../components/banner/newsBanner';
-import PopularArticles from '../components/card/popularArticle';
-import SearchArticle from '../components/form/Search';
 import TodayHeadlines from '../components/card/components/todayHeadlines';
-const Home = () => {
+import PopularArticles from '../components/card/popularArticle';
 
+const Home = () => {
     return (
-<>
+        <>
             <NewsBanner />
             <PopularArticles />
-            {/* <CaseStudies />*/}
             <TodayHeadlines />
-</>
-   )
+        </>
+    )
 }
 
 export default Home;
