46ebb8db3e740d7c438942efa13e42351e2b45a5
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-24 21:48:41 +0430

    Subject: Layout, Description: Edit layout, Date: 6/24/2023

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useState } from "react";
 
 const Layout = () => {
+    const [showMenu, setShowMenu] = useState(false)
+
+    console.log(showMenu);
     return (
         <>
-            <Navbar />
+            <div className={`overlay ${showMenu ? "show" : ""}`} onClick={()=> setShowMenu(false)}></div>
+            <Navbar showMenu={showMenu} setShowMenu={setShowMenu}/>
             <main className="xl:container mx-auto xl:px-0 px-4">
-            <Outlet />
+                <div className="xl:w-10/12 lg:w-10/12 md:w-full mx-auto">
+                    <Outlet />
+                </div>
             </main>
             <Footer />
         </>
