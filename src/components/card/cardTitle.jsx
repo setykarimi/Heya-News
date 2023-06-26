3840678e7ac3d6f1c048c20403844ed7e47edf12
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-26 21:12:28 +0430

    Subject: Style, Description: Edit classNames, Date: 6/25/2023

--- src/components/card/cardTitle.jsx
+++ src/components/card/cardTitle.jsx
@@ -1,7 +1,7 @@
 const CardTitle = ({title, description}) => {
     return ( <div className="bg-gray-200 p-10 rounded-xl text-center">
-        <span className="font-bold text-3xl text-gray-800 block">{title}</span>
-        <span className="text-gray-500 block font-light mt-2">{description}</span>
+        <span className="font-bold lg:text-5xl text-3xl text-gray-800 block">{title}</span>
+        <span className="text-gray-500 block font-light mt-3">{description}</span>
     </div> );
 }
  
