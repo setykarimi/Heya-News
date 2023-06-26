c18efe42d45c9e9bf9452cd47f282b7b52a710e4
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-26 21:46:46 +0430

    Subject: Search, Description: navigate to news page on searching, Date: 6/25/2023

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,8 +1,10 @@
 import { CloseCircle, Menu, SearchNormal1 } from 'iconsax-react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../assets/images/Logo.svg';
 const Navbar = ({ showMenu, setShowMenu }) => {
+    const inputRef = useRef()
+    const navigate = useNavigate()
     const menuItem = [
         {
             title: 'Home',
@@ -30,6 +32,14 @@ const Navbar = ({ showMenu, setShowMenu }) => {
         setShowMenu(!showMenu)
     }
 
+    const searchHandler = () => {
+        navigate('/news', {
+            state: {
+            search: inputRef.current.value
+            }
+        })
+    }
+
     return (
         <nav className="mx-auto sticky shadow-sm top-0 text-gray-900 bg-white xl:px-12 px-6 py-4 mb-10" style={{zIndex: '9999'}}>
             <div className="xl:container mx-auto flex justify-between items-center">
@@ -39,12 +49,12 @@ const Navbar = ({ showMenu, setShowMenu }) => {
                     <div className=' w-full mx-auto '>
                         <div className="items-center bg-gray-100 rounded-full p-1 flex gap-2 ">
                             <SearchNormal1 size="24" color="#959EAD" className='ml-2' />
-                            <input className="w-full py-1 px-2 bg-gray-100 outline-none text-gray-700 placeholder:text-gray-600 placeholder:text-sm" type='search' placeholder='Search article' />
-                            <button className="bg-blue-500 h-full text-white py-2 px-4 text-sm rounded-full font-light">Search</button>
+                            <input className="w-full py-1 px-2 bg-gray-100 outline-none text-gray-700 placeholder:text-gray-600 placeholder:text-sm" type='search' placeholder='Search article' ref={inputRef}/>
+                            <button className="bg-blue-500 h-full text-white py-2 px-4 text-sm rounded-full font-light" onClick={searchHandler}>Search</button>
                         </div>
                     </div>
                     {menuItem.map(({ title, link }) => <Link key={title} to={link} className='font-light'>{title}</Link>)}
-                    <button className='border border-gray-200 whitespace-nowrap py-2 px-6 ml-4 rounded-full font-medium'>Get it Now</button>
+                    <button className='border border-gray-200 whitespace-nowrap py-2 px-6 ml-4 rounded-full font-medium'>Login</button>
                 </div>
 
                 <button className='lg:hidden' onClick={showMenuHandler}>
