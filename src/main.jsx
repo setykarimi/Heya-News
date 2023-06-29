efb1b64ea8b38c36558bea8d96ba32e9e75552e0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-29 16:44:48 +0430

    Subject: Redux, Description: Add redux provider, Date: 6/29/2023

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,8 @@ import TodayHeadlines from 'Pages/TodayHeadlines';
 import Login from 'Pages/Login';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { Provider } from 'react-redux'
+import { store } from 'Services/store';
 
 const router = createBrowserRouter([
   {
@@ -50,17 +51,19 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ToastContainer position="top-center"
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable
-        pauseOnHover={false}
-        theme="colored" />
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ToastContainer position="top-center"
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover={false}
+          theme="colored" />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>,
 )
