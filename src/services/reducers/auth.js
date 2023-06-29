52d30446e0b89271d3746d5a497f0a006e6379e5
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-29 16:45:19 +0430

    Subject: Redux, Description: Create store and reducer for saving auth data, Date: 6/29/2023

---
+++ src/services/reducers/auth.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { Python } from 'iconsax-react'
+
+const initialState = {
+    email: JSON.parse(localStorage.getItem('auth'))?.email,
+    password: JSON.parse(localStorage.getItem('auth'))?.password
+}
+
+export const AuthSLice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        ADD_USER_INFO: (state, { payload }) => {
+            const data = JSON.parse(localStorage.getItem('auth'));
+
+            state.email = data.email
+            state.password = data.password
+        }
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { ADD_USER_INFO } = AuthSLice.actions
+
+export default AuthSLice.reducer
