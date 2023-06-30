ea3e4b961ca9e410fabe60e317004ccdff045311
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-30 14:06:25 +0430

    Subject: ToastContainer, Description: Add toastContainer, Date: 6/30/2023

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { useState } from "react";
+import { ToastContainer } from "react-toastify";
 
 const Layout = () => {
     const [showMenu, setShowMenu] = useState(false)
@@ -9,8 +10,17 @@ const Layout = () => {
     console.log(showMenu);
     return (
         <>
-            <div className={`overlay cursor-pointer ${showMenu ? "show" : ""}`} onClick={()=> setShowMenu(false)}></div>
-            <Navbar showMenu={showMenu} setShowMenu={setShowMenu}/>
+            <ToastContainer position="top-center"
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable
+                pauseOnHover={false}
+                theme="colored" />
+            <div className={`overlay cursor-pointer ${showMenu ? "show" : ""}`} onClick={() => setShowMenu(false)}></div>
+            <Navbar showMenu={showMenu} setShowMenu={setShowMenu} />
             <main className="xl:container mx-auto xl:px-0 px-4">
                 <div className="xl:w-10/12 lg:w-10/12 md:w-full mx-auto">
                     <Outlet />
