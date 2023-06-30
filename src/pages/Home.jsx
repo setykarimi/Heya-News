3f8f8b8650ad907b132914d49b0dc58f289e5cfc
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-30 13:41:27 +0430

    Subject: MultiLanguage, Description: Add language data for test, Date: 6/30/2023

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import NewsBanner from "Components/banner/newsBanner";
 import TodayHeadlines from "Cards/components/todayHeadlines";
 import PopularArticles from "Cards/popularArticle";
+import { useTranslation } from "react-i18next";
 
 
 const Home = () => {
+    const { t } = useTranslation();
+
     return (
         <>
+        <h1>{t("welcome")}</h1>
             <NewsBanner />
             <PopularArticles />
             <TodayHeadlines />
