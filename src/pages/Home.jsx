d6ad7d582bab43d22858d5ed9c39f5d7072b1561
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-30 14:04:52 +0430

    Subject: MultiLanguage, Description: Remove translation from home page, Date: 6/30/2023

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
-import NewsBanner from "Components/banner/newsBanner";
 import TodayHeadlines from "Cards/components/todayHeadlines";
 import PopularArticles from "Cards/popularArticle";
-import { useTranslation } from "react-i18next";
-
+import NewsBanner from "Components/banner/newsBanner";
 
 const Home = () => {
-    const { t } = useTranslation();
-
     return (
         <>
-        <h1>{t("welcome")}</h1>
+       
             <NewsBanner />
             <PopularArticles />
             <TodayHeadlines />
