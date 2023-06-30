316a4a9babb1bb7fe291de7d9230cfada634d9be
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-06-30 14:06:13 +0430

    Subject: ToastContainer, Description: Remove toastContainer, Date: 6/30/2023

--- src/main.jsx
+++ src/main.jsx
@@ -56,15 +56,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <I18nextProvider i18n={i18n}>
       <Provider store={store}>
         <QueryClientProvider client={queryClient}>
-          <ToastContainer position="top-center"
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss={false}
-            draggable
-            pauseOnHover={false}
-            theme="colored" />
           <RouterProvider router={router} />
         </QueryClientProvider>
       </Provider>
