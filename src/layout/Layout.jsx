8f646601be623fa59b42897035ad55527cb0d23a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-02 21:22:32 +0430

    Subject: ToastContainer, Description: Remove toastContainer, Date: 7/2/2023

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,8 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
 import { useState } from "react";
-import { ToastContainer } from "react-toastify";
 import { useTranslation } from "react-i18next";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 const Layout = () => {
     const [showMenu, setShowMenu] = useState(false)
@@ -11,15 +10,7 @@ const Layout = () => {
 
     return (
         <div style={{direction: i18n.language == "fa" ? "rtl":"ltr", fontFamily: i18n.language == "fa"  && "Calibri"}}>
-            <ToastContainer position="top-center"
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss={false}
-                draggable
-                pauseOnHover={false}
-                theme="colored" />
+           
             <div className={`overlay cursor-pointer ${showMenu ? "show" : ""}`} onClick={() => setShowMenu(false)}></div>
             <Navbar showMenu={showMenu} setShowMenu={setShowMenu} />
             <main className="xl:container mx-auto xl:px-0 px-4">
