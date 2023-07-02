df0bf1d37d67e2ca51bc362d74c9ed4463113f8d
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-02 21:22:00 +0430

    Subject: Dashboard, Description: Create new route for profile, Date: 7/2/2023

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,24 @@
+import Home from 'Pages/Home';
+import Login from 'Pages/Login';
+import News from 'Pages/News';
+import PopularNews from 'Pages/PopularNews';
+import TodayHeadlines from 'Pages/TodayHeadlines';
+import { store } from 'Services/store';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { I18nextProvider } from 'react-i18next';
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from 'react-redux';
 import {
   RouterProvider,
   createBrowserRouter
 } from "react-router-dom";
-import './index.css';
-import Layout from './layout/Layout';
-import News from 'Pages/News';
-import Home from 'Pages/Home';
-import PopularNews from 'Pages/PopularNews';
-import TodayHeadlines from 'Pages/TodayHeadlines';
-import Login from 'Pages/Login';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Provider } from 'react-redux'
-import { store } from 'Services/store';
-import { I18nextProvider } from 'react-i18next';
 import i18n from '../i18n';
+import './index.css';
+import Layout from './layout/Layout';
+import DashboardLayout from 'Layout/dashboard/Layout';
 
 const router = createBrowserRouter([
   {
@@ -45,8 +46,17 @@ const router = createBrowserRouter([
         element: <TodayHeadlines />,
       },
     ]
+  },
+  // Profile Dashboard
+  {
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/profile",
+        element: <Home />,
+      }
+    ]
   }
-
 ]);
 
 const queryClient = new QueryClient();
@@ -56,6 +66,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <I18nextProvider i18n={i18n}>
       <Provider store={store}>
         <QueryClientProvider client={queryClient}>
+          <ToastContainer position="top-center"
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable
+            pauseOnHover={false}
+            theme="colored" />
           <RouterProvider router={router} />
         </QueryClientProvider>
       </Provider>
