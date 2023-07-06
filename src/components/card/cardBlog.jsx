f462410544baff2a971167f71c6b9904386748c6
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-06 11:46:13 +0430

    Subject: Dashboard, Description: Create new component for cardBlogs, Date: 7/6/2023

---
+++ src/components/card/cardBlog.jsx
@@ -0,0 +1,16 @@
+import { Edit } from "iconsax-react";
+
+const CardBlog = ({ img, title, content }) => {
+    return (
+        <div className="bg-white border border-gray-200 rounded-xl p-4 flex items-center gap-4">
+            <img src={img} className="rounded-full w-16 h-16 object-cover" alt={title} />
+            <div>
+                <span className="block font-medium">{title}</span>
+                <p className="text-sm line-clamp-1">{content}</p>
+            </div>
+            <Edit size="24" color="#3B82F6" variant="TwoTone"
+            />
+        </div>);
+}
+
+export default CardBlog;
