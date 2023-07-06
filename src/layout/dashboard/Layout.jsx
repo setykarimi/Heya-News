b6f76a2d7f4646d8254d487f15aef1946b4bf516
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-06 11:11:51 +0430

    Subject: Style, Description: Add CardLanguage, Date: 7/6/2023

--- src/layout/dashboard/Layout.jsx
+++ src/layout/dashboard/Layout.jsx
@@ -1,3 +1,4 @@
+import CardLanguage from "Components/card/cardLanguage";
 import CardProfile from "Components/card/cardProfile";
 import LatestNews from "Components/card/components/latestNews";
 import MyLikes from "Components/card/components/myLikes";
@@ -5,14 +6,17 @@ import MyLikes from "Components/card/components/myLikes";
 const DashboardLayout = () => {
     return (
         <main className="xl:container mx-auto p-4 ">
-            <div className="grid grid-cols-5 gap-4 ">
-                <CardProfile />
+            <div className="grid grid-cols-5 gap-4">
+                <div>
+                    <CardProfile />
+                    <CardLanguage />
+                </div>
                 <div className="col-span-3">
                     <LatestNews />
                 </div>
-                <div>
+              
                     <MyLikes />
-                </div>
+               
             </div>
         </main>
     );
