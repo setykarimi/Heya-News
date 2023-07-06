f4ddc6c5dd99f27ddad8cb2dd246323b2690ef39
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-06 22:31:05 +0430

    Subject: MultiLanguage, Description: Edit content for multiLanguage, Date: 7/6/2023

--- src/components/banner/newsBanner.jsx
+++ src/components/banner/newsBanner.jsx
@@ -1,23 +1,28 @@
 import { Link } from 'react-router-dom';
 import Image from 'Images/Banner.png';
 import avatar from 'Images/people/boyOne.png';
+import { useTranslation } from 'react-i18next';
 
 const NewsBanner = () => {
+    const { t } = useTranslation();
+
     return (
         <div className='p-1 rounded-2xl bg-white'>
             <div className="md:grid flex flex-col grid-cols-2">
                 <img src={Image} alt="NewsBanner" loading="lazy" className='rounded-2xl md:h-full h-60 object-cover' />
                 <div className='flex flex-col gap-2 justify-between p-6'>
-                    <h2 className='text-gray-950 font-bold xl:text-5xl md:text-2xl text-xl'>Cheap Airline Tickets Great Ways To Save</h2>
-                    <p className='text-gray-800 font-light text-justify'><span className='font-medium block'>In this digital generation where information can be easily obtained within seconds, business cards since the 1500s.</span> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Dived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.  It has survived not only five centuries, but also the leap into electronic typesetting, remaining not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Dived not only five  <Link to="" className='font-medium'>Read more ...</Link></p>
+                    <h2 className='text-gray-950 font-bold xl:text-4xl md:text-2xl text-xl'>{t('banner.title')}</h2>
+                    <p className='text-gray-800 font-light text-justify'>
+                    {t('banner.content')}
+                        <Link to="" className='font-medium'> {t('read_more')}...</Link></p>
                     <div className='flex justify-between items-center'>
                         <div className='flex items-center gap-2'>
                             <img src={avatar} alt="avatar" className='rounded-full h-8 w-8 object-cover'/>
                             <div>
-                                <span className='block font-medium text-sm'>Viola Manisa</span>
+                                <span className='block font-medium text-sm'> {t('banner.author')}</span>
                             </div>
                         </div>
-                        <span className='text-sm text-gray-400'>02 May</span>
+                        <span className='text-sm text-gray-400'> {t('banner.date')}</span>
                     </div>
                 </div>
             </div>
