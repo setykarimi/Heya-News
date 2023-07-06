76ee948d3be2ec3a1b8df8b5c2938eb5c1b28063
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-06 14:29:07 +0430

    Subject: CardLike, Description: like handler, Date: 7/6/2023

--- src/components/card/cardLike.jsx
+++ src/components/card/cardLike.jsx
@@ -1,15 +1,18 @@
 import { Heart } from "iconsax-react";
+import { useState } from "react";
 
-const CardLike = ({
-    img,
-    content
-}) => {
+const CardLike = ({ img, content }) => {
+    const [like, setLike] = useState(true)
+    const likeHandler = () => {
+        setLike(!like)
+    }
     return (
         <div className="flex gap-2 justify-between w-full p-4 items-center border border-gray-200 bg-white rounded-lg">
             <img src={img} loading="lazy" className="w-16 h-16 rounded-lg object-cover" alt="likes" />
             <p className="xl:text-xs line-clamp-4 text-justify">{content}</p>
-            <button>
-                <Heart size="15" variant="Bold" color="#EF4444" /></button>
+            <button onClick={likeHandler}>
+                <Heart size="15" variant={like ? "Bold" : "Outline"} color="#EF4444" />
+            </button>
         </div>
     );
 }
