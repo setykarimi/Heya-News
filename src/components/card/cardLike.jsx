cdcd8b8684d6b50447f50deeaae1659dfa747732
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-06 13:59:14 +0430

    Subject: Lazy loading, Description: add loading lazy to img tag, Date: 7/6/2023

--- src/components/card/cardLike.jsx
+++ src/components/card/cardLike.jsx
@@ -6,7 +6,7 @@ const CardLike = ({
 }) => {
     return (
         <div className="flex gap-2 justify-between w-full p-4 items-center border border-gray-200 bg-white rounded-lg">
-            <img src={img} className="w-16 h-16 rounded-lg object-cover" alt="likes" />
+            <img src={img} loading="lazy" className="w-16 h-16 rounded-lg object-cover" alt="likes" />
             <p className="xl:text-xs line-clamp-4 text-justify">{content}</p>
             <button>
                 <Heart size="15" variant="Bold" color="#EF4444" /></button>
