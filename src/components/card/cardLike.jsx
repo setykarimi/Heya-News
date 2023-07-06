4512069717e0d5792c1923f2d33d63b5deba656b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-06 13:50:04 +0430

    Subject: Responsive, Description: Edit classNames for responsive view, Date: 7/6/2023

--- src/components/card/cardLike.jsx
+++ src/components/card/cardLike.jsx
@@ -5,10 +5,11 @@ const CardLike = ({
     content
 }) => {
     return (
-        <div className="flex gap-1 justify-between w-full p-4 items-center border border-gray-200 bg-white rounded-lg">
+        <div className="flex gap-2 justify-between w-full p-4 items-center border border-gray-200 bg-white rounded-lg">
             <img src={img} className="w-16 h-16 rounded-lg object-cover" alt="likes" />
-            <p className="text-xs line-clamp-4 text-justify w-1/2">{content}</p>
-            <button><Heart size="15" variant="Bold" color="#EF4444" /></button>
+            <p className="xl:text-xs line-clamp-4 text-justify">{content}</p>
+            <button>
+                <Heart size="15" variant="Bold" color="#EF4444" /></button>
         </div>
     );
 }
