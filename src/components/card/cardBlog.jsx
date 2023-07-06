75afdcb9d92f9eb936206111f3842f00c58a5446
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-06 13:58:39 +0430

    Subject: Lazy loading, Description: add loading lazy to img tag, Date: 7/6/2023

--- src/components/card/cardBlog.jsx
+++ src/components/card/cardBlog.jsx
@@ -3,7 +3,7 @@ import { Edit } from "iconsax-react";
 const CardBlog = ({ img, title, content }) => {
     return (
         <div className="bg-white border border-gray-200 rounded-xl p-4 flex items-center gap-4">
-            <img src={img} className="rounded-full w-16 h-16 object-cover" alt={title} />
+            <img src={img} loading="lazy" className="rounded-full w-16 h-16 object-cover" alt={title} />
             <div>
                 <span className="block font-medium">{title}</span>
                 <p className="text-sm line-clamp-1">{content}</p>
