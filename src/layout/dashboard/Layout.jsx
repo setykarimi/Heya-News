9893cc92d728b1b1361d1caacbcc3af60216519f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-06 10:48:08 +0430

    Subject: Dashboard, Description: Add myLikes, Date: 7/6/2023

--- src/layout/dashboard/Layout.jsx
+++ src/layout/dashboard/Layout.jsx
@@ -1,17 +1,18 @@
 import CardProfile from "Components/card/cardProfile";
 import LatestNews from "Components/card/components/latestNews";
+import MyLikes from "Components/card/components/myLikes";
 
 const DashboardLayout = () => {
     return (
         <main className="xl:container mx-auto p-4 ">
             <div className="grid grid-cols-5 gap-4 ">
-            <CardProfile />
-            <div className="col-span-3">
-               <LatestNews />
-            </div>
-            <div>
-               
-            </div>
+                <CardProfile />
+                <div className="col-span-3">
+                    <LatestNews />
+                </div>
+                <div>
+                    <MyLikes />
+                </div>
             </div>
         </main>
     );
