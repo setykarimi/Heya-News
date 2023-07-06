99e3e407aa98e0918199e0f5d7d69c92bd6af3eb
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-06 13:48:44 +0430

    Subject: Profile page, Description: Create new component for profile page, Date: 7/6/2023

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,31 @@
+import CardLanguage from "Components/card/cardLanguage";
+import CardProfile from "Components/card/cardProfile";
+import LatestNews from "Components/card/components/latestNews";
+import MyBlog from "Components/card/components/myBlogs";
+import MyLikes from "Components/card/components/myLikes";
+import Navbar from "Layout/Navbar";
+
+const Profile = () => {
+    return (
+        <>
+            <Navbar />
+            <main className="xl:container mx-auto p-4 ">
+                <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-4">
+                    <div>
+                        <CardProfile />
+                        <CardLanguage />
+                    </div>
+                    <div className="lg:col-span-3 md:col-span-2">
+                        <LatestNews />
+                        <MyBlog />
+                    </div>
+                    <div className="xl:col-span-1 lg:col-span-4 md:col-span-3">
+                    <MyLikes />
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+}
+
+export default Profile;
