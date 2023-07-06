ec08323e53c05521feda5f7c5ba3b74e7d1ccb79
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-06 10:49:37 +0430

    Subject: Dashboard, Description: Edit style, Date: 7/6/2023

--- src/components/card/components/latestNews.jsx
+++ src/components/card/components/latestNews.jsx
@@ -1,9 +1,9 @@
 import motherHood from 'Images/motherhood/motherhood.png';
 import camping from 'Images/travel/camping.png';
 import friends from 'Images/university/freinds.png';
-import { CardArticle } from "../cardArticle";
+import { Link2 } from 'iconsax-react';
 import { Link } from 'react-router-dom';
-import { Link1, Link2 } from 'iconsax-react';
+import { CardArticle } from "../cardArticle";
 
 const LatestNews = () => {
 
@@ -17,7 +17,7 @@ const LatestNews = () => {
         {
             date: "2020 - 2022",
             title: "Camping Up: It's about time we redefine what ",
-            content:"A Christian-owned wellness center, Braidwood, is exempted from the federal law prohibiting employers from discriminating on the basis of sexual orientation and gender identity, a federal appeals court ruled last week.",
+            content: "A Christian-owned wellness center, Braidwood, is exempted from the federal law prohibiting employers from discriminating on the basis of sexual orientation and gender identity, a federal appeals court ruled last week.",
             img: camping
         },
         {
@@ -27,20 +27,22 @@ const LatestNews = () => {
             img: friends
         },
     ]
-    return ( 
+    return (
         <div className="bg-white rounded-xl">
-            <h2 className='text-xl text-blue-gray-950 font-bold mb-4 p-8 border-b border-gray-100'>Latest News</h2>
+            <h2 className='text-xl text-blue-gray-950 font-bold mb-4 p-8 border-b border-gray-100'>
+                Latest News
+            </h2>
             <div className="grid grid-cols-3 gap-8 p-8">
-                {news.map((item)=> <CardArticle data={item}/>)}
+                {news.map((item) => <CardArticle data={item} />)}
             </div>
             <div className='flex items-center justify-center pb-4'>
                 <Link to="" className='text-sm flex items-center gap-1 text-gray-700 font-light'>
-                    <Link2 color='#3B82F6' size="20"/>
+                    <Link2 color='#3B82F6' size="20" />
                     See More
                 </Link>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default LatestNews;
