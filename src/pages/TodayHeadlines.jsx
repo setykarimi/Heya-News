c14a5d59a4976682e83cc2a18cef011600ed74de
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-13 12:05:51 +0430

    Subject: Multilanguuage, Description: Edit data for multiLanguage, Date: 7/13/2023

--- src/pages/TodayHeadlines.jsx
+++ src/pages/TodayHeadlines.jsx
@@ -1,24 +1,33 @@
 import CardNewsDetail from "Cards/cardNewsDetail";
 import CardTitle from "Cards/cardTitle";
 import axios from "axios";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { useQuery } from "react-query";
 
 const TodayHeadlines = () => {
+    const { t } = useTranslation();
+
     async function fetchData() {
         const { data } = await axios.get(`https://techcrunch.com/wp-json/wp/v2/posts?per_page=5&context=embed&search=today`);
         return data;
     }
+
     const { data, isError, isLoading } = useQuery("news", fetchData);
+
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     return (
         <div>
-            <CardTitle title="Today Headlines" description="Top today news in the US right now" />
+            <CardTitle title={t("today_headlines.title")} description={t("today_headlines.desc")} />
             <div className="flex flex-col gap-8 mt-12">
-            {isLoading ? <div>Loading</div> : isError ? <div>Error</div> :
-                data.map((item, index) => <CardNewsDetail key={item.id} data={item} index={index}/>)}
-                </div>
+                {isLoading ? <div>Loading</div> : isError ? <div>Error</div> :
+                    data.map((item, index) => <CardNewsDetail key={item.id} data={item} index={index} />)}
+            </div>
         </div>
     );
 }
- 
+
 export default TodayHeadlines;
