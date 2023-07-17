d3e8b6ae9f1ddc08326b98f8a1a6b8e295cb5c1d
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-17 19:56:32 +0430

    Subject: MultiLanguage, Description: Edit data for multiLanguage, Date: 7/17/2023

--- src/components/card/components/myLikes.jsx
+++ src/components/card/components/myLikes.jsx
@@ -2,30 +2,33 @@ import CardLike from "../cardLike";
 import img1 from 'Images/travel/rent.png'
 import img2 from 'Images/university/friendship.png'
 import img3 from 'Images/university/university.png'
+import { useTranslation } from "react-i18next";
 
 const MyLikes = () => {
+    const { t } = useTranslation()
+
     const like = [
         {
             img: img1,
-            content: "The attacker faces sentencing after pleading guilty to federal hate crimes charges. "
+            content: t("my_likes.like2")
         },
         {
             img: img2,
-            content: "Emotional testimony from survivors and victims’ families began on Wednesday in the federal sentencing hearing"
+            content: t("my_likes.like2")
         },
         {
             img: img3,
-            content: "In the federal case, where testimony on a possible sentence was expected to last at least two days, prosecutors agreed with the"
+            content: t("my_likes.like3")
         }
     ]
     return (
         <div className="bg-white rounded-xl h-fit">
             <h2 className='text-xl text-blue-gray-950 font-bold mb-4 md:p-8 p-4 border-b border-gray-100'>
-                My Likes
+                {t("my_likes.title")}
             </h2>
             <div className="p-4 grid xl:grid-cols-1 md:grid-cols-2 grid-cols-1 gap-4">
                 {like.map((item) => <CardLike key={item.content} img={item.img} content={item.content} />)}
-                </div>
+            </div>
         </div>
     );
 }
