9e70a7fa5aa490638740363e96e7497129b505f4
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-23 20:19:13 +0430

    Subject: Loading, Description: Add loading component, Date: 7/23/2023

--- src/pages/TodayHeadlines.jsx
+++ src/pages/TodayHeadlines.jsx
@@ -1,5 +1,6 @@
 import CardNewsDetail from "Cards/cardNewsDetail";
 import CardTitle from "Cards/cardTitle";
+import Loading from "Components/alert/loading";
 import axios from "axios";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
@@ -23,7 +24,7 @@ const TodayHeadlines = () => {
         <div className="xl:w-10/12 lg:w-11/12 md:w-full mx-auto">
             <CardTitle title={t("today_headlines.title")} description={t("today_headlines.desc")} />
             <div className="flex flex-col gap-8 mt-12">
-                {isLoading ? <div>Loading</div> : isError ? <div>Error</div> :
+                {isLoading ? <Loading /> : isError ? <div>Error</div> :
                     data.map((item, index) => <CardNewsDetail key={item.id} data={item} index={index} />)}
             </div>
         </div>
