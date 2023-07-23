c3c143aa34bb5e1b36fe71f26fa4237e4c8d07dd
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-07-23 21:18:33 +0430

    Subject: multiLanguage, Description: Edit content for multiLanguage, Date: 7/23/2023

--- src/components/card/components/myBlogs.jsx
+++ src/components/card/components/myBlogs.jsx
@@ -1,13 +1,15 @@
 import { useSelector } from "react-redux";
 import CardBlog from "../cardBlog";
+import { useTranslation } from "react-i18next";
 
 const MyBlog = () => {
     const {blog} = useSelector(state => state.blog)
-   
+    const { t } = useTranslation()
+
     return (
         <div className=" h-fit mt-4">
             <h2 className='text-xl text-blue-gray-950 font-bold mb-4 '>
-                My Blogs
+                {t("my_blog.title")}
             </h2>
             <div className="flex flex-col gap-4">
                 {blog.map((item) => <CardBlog key={item.id} id={item.id} img={item.img} title={item.title} content={item.content} />)}
